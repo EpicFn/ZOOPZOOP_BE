81ac6409852a7a8f2cd58d9ab6ac2f1d0df0054c
EpicFn <rlagurwls056@gmail.com>
2026-01-11 20:30:07 +0900

    feat : D예외 발생 시 DLQ 전ã이송 로직 추가

--- src/main/java/org/tuna/zoopzoop/backend/domain/dashboard/extraComponent/GraphUpdateConsumer.java
+++ src/main/java/org/tuna/zoopzoop/backend/domain/dashboard/extraComponent/GraphUpdateConsumer.java
@@ -3,6 +3,7 @@ package org.tuna.zoopzoop.backend.domain.dashboard.extraComponent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Component;
@@ -25,15 +26,14 @@ public class GraphUpdateConsumer {
             dashboardService.updateGraph(message.dashboardId(), dto);
             log.info("Successfully updated graph for dashboardId: {}", message.dashboardId());
         } catch (ObjectOptimisticLockingFailureException e) {
-            // Optimistic Lock 충돌 발생!
-            // 내가 처리하려던 메시지는 이미 구버전 데이터에 대한 요청이었음.
-            // 따라서 이 메시지는 무시하고 정상 처리된 것으로 간주.
+            // Optimistic Lock 충돌 발생
+            // 정상적인 상황으로 간주하고 무시 (DLQ 전송 안하고 큐에서 제거)
             log.warn("Stale update attempt for dashboardId: {}. A newer version already exists. Discarding message.", message.dashboardId());
-            // 예외를 다시 던지지 않으므로, 메시지는 큐에서 정상적으로 제거(ACK)됩니다.
+
         } catch (Exception e) {
-            // 실제 운영에서는 메시지를 재시도하거나, 실패 큐(Dead Letter Queue)로 보내는 등의
-            // 정교한 에러 처리 로직이 필요합니다.
-            log.error("Failed to process graph update for dashboardId: {}", message.dashboardId(), e);
+            // 비정상적인 에러 발생
+            // 메세지를 Requeue하지 않고 DLQ로 전송
+            throw new AmqpRejectAndDontRequeueException(e);
         }
     }
 }
